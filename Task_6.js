e745e5221a3528e32a69d5b232893b0e6b420393
darshhbad <darsh.badukle.che17@itbhu.ac.in>
2023-03-27 19:30:16 +0530

    server creation

---
+++ Task_6.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// function rqListener(req,res){
+
+// }
+
+// http.createServer(rqListener);
+
+const server = http.createServer((reqs,reso)=>{
+    console.log("Darsh Badukle")
+
+});
+
+server.listen(4000);
