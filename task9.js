ea47d89e34643b55c7abd35c568a6b3a03cc2726
darshhbad <darsh.badukle.che17@itbhu.ac.in>
2023-03-30 09:58:14 +0530

    body parser

--- task9.js
+++ task9.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" mehod="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</button></form>')
 })
 
 app.post('/product',(req,res,next)=>{
